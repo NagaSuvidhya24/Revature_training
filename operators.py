d27ff203c1492b2cae92edcd8155824be9549ba0
trainee nagasuvidhya <bangalorenagasuvidhya03@gmail.com>
2025-10-24 18:49:54 +0530

    Py notes commit

---
+++ operators.py
@@ -0,0 +1,16 @@
+Python 3.12.10 (tags/v3.12.10:0cc8128, Apr  8 2025, 12:21:36) [MSC v.1943 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> 5>6 and 3<7
+False
+>>> 5<6 and 3>7
+False
+>>> not 6
+False
+>>> not -6
+False
+>>> 7&9
+1
+>>> 7|9
+15
+>>> 7^9
+14
